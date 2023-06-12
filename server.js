b4a7064d74f4a4bbcd34d56a983bc45bb061f3c3
qualityseg <128048171+qualityseg@users.noreply.github.com>
2023-06-12 09:03:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -84,30 +84,7 @@ app.post('/register', (req, res) => {
 });
 
 
-mercadopago.configure({
-  access_token: 'TEST-2684905602430236-052513-51d07b1caa42a7938ab7e2a9f13a7f98-135153905',
-});
 
-app.post('/create_preference', async (req, res) => {
-  const { title, price, quantity } = req.body;
-
-  const preference = {
-    items: [
-      {
-        title,
-        unit_price: Number(price),
-        quantity: Number(quantity),
-      },
-    ],
-  };
-
-  try {
-    const response = await mercadopago.preferences.create(preference); // Correção aqui
-    res.json({ id: response.body.id });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
 
 const port = process.env.PORT || 5000;
 
